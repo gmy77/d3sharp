8e94959afd7c6abdaa652bbcfad5ec96858bb3b0
Tim Howard <hai2you2@gmail.com>
2011-09-15 15:05:06 -0400

    Removed 0x from HexDump output; simplified Dump and HexDeump methods.

--- source/D3Sharp/Utils/Extensions/EnumerableExtensions.cs
+++ source/D3Sharp/Utils/Extensions/EnumerableExtensions.cs
@@ -10,14 +10,11 @@ namespace D3Sharp.Utils.Extensions
         public static string HexDump(this IEnumerable<byte> collection)
         {
             var sb = new StringBuilder();
-
             foreach (byte value in collection)
             {
-                sb.Append("0x");
                 sb.Append(value.ToString("X2"));
                 sb.Append(' ');
             }
-
             return sb.ToString();
         }
 
@@ -28,30 +25,17 @@ namespace D3Sharp.Utils.Extensions
 
         public static string Dump(this IEnumerable<byte> collection)
         {
-            var output = Environment.NewLine;
-            var hex = string.Empty; // hex buffer. 
-            var text = string.Empty; // text buffer.
-
-            int i = 0;
-
-            foreach (byte value in collection)
-            {
-                if (i > 0 && ((i%16) == 0)) // with-in every 16 chars, put a new line.
-                {
-                    output += string.Format("{0} {1}", hex, text);
-                    hex = text = string.Empty;
-                    output += Environment.NewLine;
-                }
-
-                hex += value.ToString("X2") + " ";
-                text += string.Format("{0}", (char.IsWhiteSpace((char) value) && (char) value != ' ') ? '.' : (char) value); // pretfy the text.
-                i++;
+            var sb=new StringBuilder();
+            int i=0;
+            foreach (byte value in collection) {
+                if (i>0 && ((i%16)==0))
+                    sb.Append(Environment.NewLine);
+                sb.Append(value.ToString("X2"));
+                sb.Append(' ');
+                ++i;
             }
-
-            if (text.Length < 16) hex = hex.PadRight(48); // pad the hex representation in-case it's smaller than a regular 16 value line.
-            output += string.Format("{0} {1}", hex, text);
-            output += Environment.NewLine;
-            return output;
+            sb.Append(Environment.NewLine);
+            return sb.ToString();
         }
     }
 }
