be5c744a5f00f26d638b9d88f29ed7b22ada4ff3
raistlinthewiz <shalafiraistlin@gmail.com>
2011-09-17 18:20:25 +0300

    Implemented ProtoStringViewer gui - http://i.imgur.com/VSoPr.png Removed tools/d3watchproto as it was confusing people, it has nothing to do with server - was just dev-only stuff.. you can get it from bhfiles.com

---
+++ tools/ProtoStringViewer/FormMain.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Be.Windows.Forms;
+using Google.ProtocolBuffers;
+
+namespace ProtoStringViewer
+{
+    public partial class FormMain : Form
+    {
+        private byte[] _byteData;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            _byteData = ProtoHelpers.Utils.Conversion.Unescape(this.textBoxInput.Text);
+            this.hexBoxOutput.ByteProvider = new DynamicByteProvider(_byteData);
+        }
+
+        private void textBoxProtoType_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxProtoType.Text.Trim() == string.Empty) return;
+            
+            var type = Type.GetType(textBoxProtoType.Text);
+            if (type == null)
+            {
+                return;
+            }
+
+            var defaultMessage = MessageUtil.GetDefaultMessage(type);
+
+            IBuilder builder = defaultMessage.WeakCreateBuilderForType();
+            if (builder == null)
+            {
+                return;
+            }
+
+            builder.WeakMergeFrom(ByteString.CopyFrom(_byteData));
+            richTextBoxProto.Text = TextFormat.PrintToString(builder.WeakBuild());            
+        }
+    }
+}
