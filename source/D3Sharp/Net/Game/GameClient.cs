df0970b31b40ef1e34ee93175a8547b68b966771
BoyC <boyc@conspiracy.hu>
2011-09-24 15:33:46 +0200

    Crash revert on some systems

--- source/D3Sharp/Net/Game/GameClient.cs
+++ source/D3Sharp/Net/Game/GameClient.cs
@@ -191,10 +191,10 @@ namespace D3Sharp.Net.Game
                                 {
                                     Field0 = new Quaternion()
                                     {
-                                        Field0 = 0f,//float.Parse(data[49]),
+                                        Field0 = 1f,//float.Parse(data[49]),
                                         Field1 = new Vector3D()
                                         {
-                                            Field0 = 1f,//float.Parse(data[46]),
+                                            Field0 = 0f,//float.Parse(data[46]),
                                             Field1 = 0f,//float.Parse(data[47]),
                                             Field2 = 0f,//float.Parse(data[48]),
                                         },
@@ -223,10 +223,10 @@ namespace D3Sharp.Net.Game
                                 {
                                     Field0 = new Quaternion()
                                     {
-                                        Field0 = 0f,//float.Parse(data2[5]),
+                                        Field0 = 1f,//float.Parse(data2[5]),
                                         Field1 = new Vector3D()
                                         {
-                                            Field0 = 1f,//float.Parse(data2[2]),
+                                            Field0 = 0f,//float.Parse(data2[2]),
                                             Field1 = 0f,//float.Parse(data2[3]),
                                             Field2 = 0f,//float.Parse(data2[4]),
                                         },
