e83596cbb3f711a1b232d200e90273107e9d1512
dark0ne <dark0ne@df.net>
2011-11-01 12:03:11 +0100

    GameAttribute enconding fixed

--- src/Mooege/Net/GS/Message/GameAttribute.List.cs
+++ src/Mooege/Net/GS/Message/GameAttribute.List.cs
@@ -174,7 +174,7 @@ namespace Mooege.Net.GS.Message
         public static readonly GameAttributeI Crit_Percent_Bonus_Capped = new GameAttributeI(166, 0, -1, 0, 0, "", "", "Crit_Percent_Bonus_Capped", GameAttributeEncoding.Int, 9, 0, 0, 32);
         public static readonly GameAttributeI Crit_Percent_Bonus_Uncapped = new GameAttributeI(167, 0, -1, 0, 0, "", "", "Crit_Percent_Bonus_Uncapped", GameAttributeEncoding.Int, 9, 0, 0, 32);
         public static readonly GameAttributeI Crit_Percent_Cap = new GameAttributeI(168, 0, -1, 0, 0, "", "", "Crit_Percent_Cap", GameAttributeEncoding.Int, 9, 0, 0, 32);
-        public static readonly GameAttributeF Crit_Damage_Percent = new GameAttributeF(169, 0, -1, 0, 0, "", "", "Crit_Damage_Percent", GameAttributeEncoding.Float16, 9, 0, 0, 16);
+        public static readonly GameAttributeF Crit_Damage_Percent = new GameAttributeF(169, 0f, 0, 0, 0, "", "", "Crit_Damage_Percent", GameAttributeEncoding.Float32, 9, 0f, 0f, 32);
         public static readonly GameAttributeI Crit_Effect_Time = new GameAttributeI(170, 0, -1, 3, 1, "", "", "Crit_Effect_Time", GameAttributeEncoding.Int, 9, 0, 0, 32);
         public static readonly GameAttributeF Pierce_Chance = new GameAttributeF(171, 0f, -1, 0, 0, "", "", "Pierce_Chance", GameAttributeEncoding.Float16, 9, 0f, 0f, 16);
         public static readonly GameAttributeF Damage_Absorb_Percent = new GameAttributeF(172, 0f, -1, 0, 0, "", "", "Damage_Absorb_Percent", GameAttributeEncoding.Float16, 9, 0f, 0f, 16);
