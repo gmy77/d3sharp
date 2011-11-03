7d50e57315d40afb709f86f5966359143ff2c408
fasbat <fasbat@i.love.rabbits>
2011-11-03 15:31:41 +0100

    Messages.

---
+++ src/Mooege/Net/GS/Message/Definitions/Hireling/HirelingInventoryMessage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mooege.Net.GS.Message.Definitions.Hireling
+{
+    [Message(Opcodes.HirelingInventoryMessage, Consumers.SelectedNPC)]
+    public class HirelingInventoryMessage : GameMessage
+    {
+        public HirelingInventoryMessage()
+            : base(Opcodes.HirelingInventoryMessage)
+        {
+        }
+
+        public override void Parse(GameBitBuffer buffer)
+        {
+        }
+
+        public override void Encode(GameBitBuffer buffer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AsText(StringBuilder b, int pad)
+        {
+            b.Append(' ', pad);
+            b.AppendLine("HirelingInventoryMessage:");
+            b.Append(' ', pad++);
+            b.AppendLine("{");
+            b.Append(' ', --pad);
+            b.AppendLine("}");
+        }
+    }
+}
