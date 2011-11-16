d08f1097e9703e2f481564affbff7ec60dcc3e7e
raistlinthewiz <shalafiraistlin@gmail.com>
2011-11-16 13:52:45 +0200

    Trying to fix exchange-service but still no-chance.

--- contrib/protocompiler/exchange-service.bat
+++
@@ -1,5 +0,0 @@
-CD definitions
-
-"../compilers/protogen_csharp.exe" --proto_path=./ --include_imports -expand_namespace_directories=true -cls_compliance=false -ignore_google_protobuf=true -service_generator_type=GENERIC -output_directory=../bin/csharp/ service\exchange\definition\exchange.proto 
-
-pause
