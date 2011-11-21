b2f322ae2a4cbfd60235092aeaa7c13c1ffe4ee0
farmy <the-hero@gmx.net>
2011-11-21 13:15:22 +0100

    Added lazy Asset loading

--- src/Mooege/Common/MPQ/LazyAsset.cs
+++ src/Mooege/Common/MPQ/LazyAsset.cs
@@ -24,27 +24,32 @@ using Mooege.Core.GS.Common.Types.SNO;
 
 namespace Mooege.Common.MPQ
 {
-    public class Asset
+    public class LazyAsset : Asset
     {
-        public SNOGroup Group {get; private set;}
-        public Int32 SNOId {get; private set;}
-        public string Name {get; private set;}
-        public string FileName {get; private set;}
-        public virtual FileFormat Data {get; private set;}
-
-        public Asset(SNOGroup group, Int32 snoId, string name)
+        public override FileFormat Data
         {
-            this.Data = null;
-            this.Group = group;
-            this.SNOId = snoId;
-            this.Name = name;
-            this.FileName = group + "\\" + this.Name + FileExtensions.Extensions[(int)group];
+            get
+            {
+                if (_data == null)
+                    _data = (FileFormat)Activator.CreateInstance(_parser, new object[] { _file });
+                return _data;
+            }
         }
 
-        public virtual void RunParser(Type parser, MpqFile file)
+        private FileFormat _data;
+        private Type _parser;
+        private MpqFile _file;
+
+        public LazyAsset(SNOGroup group, Int32 snoId, string name)
+            : base(group, snoId, name)
         {
+        }
+
+        public override void RunParser(Type parser, MpqFile file)
+        {
+            _parser = parser;
+            _file = file;
             Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture; // Use invariant culture so that we don't hit pitfalls in non en/US systems with different number formats.
-            this.Data = (FileFormat) Activator.CreateInstance(parser, new object[] {file});
         }
     }
 }
