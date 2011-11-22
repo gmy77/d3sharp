f1260901915534c0317ecb3e6a62485c508c1cff
raistlinthewiz <shalafiraistlin@gmail.com>
2011-11-22 22:42:03 +0200

    Added an Assembly file also to LibMooNet and versioned it as 7841.

--- src/LibMooNet/Properties/AssemblyInfo.cs
+++ src/LibMooNet/Properties/AssemblyInfo.cs
@@ -18,13 +18,12 @@
 
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using Mooege.Common.Versions;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("mooege")]
-[assembly: AssemblyDescription("mooege")]
+[assembly: AssemblyTitle("LibMooege")]
+[assembly: AssemblyDescription("LibMooege")]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else
@@ -40,7 +39,7 @@ using Mooege.Common.Versions;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion(VersionInfo.Assembly.Version)]
+[assembly: AssemblyVersion("0.7841.*")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
