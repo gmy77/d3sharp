f195ec616f1a96fe594d148be2b5e8790b93517a
raistlinthewiz <shalafiraistlin@gmail.com>
2012-02-27 02:01:27 +0200

    minor updates on VersionInfo.cs file.

--- src/Mooege/Common/Versions/VersionInfo.cs
+++ src/Mooege/Common/Versions/VersionInfo.cs
@@ -74,10 +74,10 @@ namespace Mooege.Common.Versions
                 /// <summary>
                 /// AchievementFile hash.
                 /// </summary>
-                //public static string AchievementFileHash = "ef29e59b9394e7c6f694afbb92b70a74c4fd4c96961a8ec490e770371b72e6ab";
+                public static string AchievementFileHash = "9c1b0943a8e68352bb60ec872f35c645036feaabaac92ea13bee8b2f1dc9c5b9"; //8610
+                //public static string AchievementFileHash = "ef29e59b9394e7c6f694afbb92b70a74c4fd4c96961a8ec490e770371b72e6ab"; // ??
                 //public static string AchievementFileHash = "0b61aeee74bba6ba02b93c9e15089404daf5d3cd1c7e631d7c108685894b3feb"; //8101
                 //public static string AchievementFileHash = "c06c3a43f760b9ef2c7965ac229531d17e93279cd2666bf1b9f130b8db5cb2f9"; //8296
-                public static string AchievementFileHash = "9c1b0943a8e68352bb60ec872f35c645036feaabaac92ea13bee8b2f1dc9c5b9"; //8610
 
                 /// <summary>
                 /// AchievementFile filename.
@@ -113,6 +113,7 @@ namespace Mooege.Common.Versions
             /// </summary>
             public const int ProtocolHash = unchecked((int)0xA8F17EC5);
 
+            // old hashes
             // unchecked((int)0xA8F17EC5)   // 8610
             // 0x01A64B41                   // 8296, 8350, 8392
             // 0xBA957E6B                   // 8059, 8101
@@ -124,15 +125,18 @@ namespace Mooege.Common.Versions
             //This is the server version sent in VersionsMessage
             public const string VersionString = "0.7.0.8619";
 
+            // old version strings.
             // 0.7.0.8619 // 8610 patch 13
             // 0.6.2.8392 // 8392 patch 12
             // 0.6.1.8350 // 8350 patch 11
             // 0.6.0.8318 // 8296 patch 10
             // 0.5.1.8115 // 8101 patch 9
             // 0.5.0.8059 // 8059 patch 8
-            // 0.3.1.7779 // 7728
-            // 0.3.0.7484 // 7447
-            // 0.3.0.7333 // beta patch-3
+            // ??         // 7931 patch 7
+            // ??         // 7841 patch 6
+            // 0.3.1.7779 // 7728 patch 5
+            // 0.3.0.7484 // 7447 patch 4
+            // 0.3.0.7333 // 7733 patch 3
         }
     }
 }
