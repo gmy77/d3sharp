83111f643927fbf10df03b7119d97e5236b44297
RedBlueKnight <janiszewski.krzysztof@yahoo.pl>
2012-04-27 08:02:28 +0200

    Undo last changes

--- src/Mooege/Core/GS/Map/Scene.cs
+++ src/Mooege/Core/GS/Map/Scene.cs
@@ -158,19 +158,17 @@ namespace Mooege.Core.GS.Map
             if (!MPQStorage.Data.Assets[SNOGroup.Scene].ContainsKey(this.SceneSNO.Id))
             {
                 Logger.Debug("AssetsForScene not found in MPQ Storage:Scene:{0}, Asset:{1}", SNOGroup.Scene,this.SceneSNO.Id);
-                //return;
+                return;
             }
-			else
-			{
-				var data = MPQStorage.Data.Assets[SNOGroup.Scene][this.SceneSNO.Id].Data as Mooege.Common.MPQ.FileFormats.Scene;
-				if (data == null) return;
-				this.AABBBounds = data.AABBBounds;
-				this.AABBMarketSetBounds = data.AABBMarketSetBounds;
-				this.NavMesh = data.NavMesh;
-				this.MarkerSets = data.MarkerSets;
-				this.LookLink = data.LookLink;
-				this.NavZone = data.NavZone;
-			}
+            var data = MPQStorage.Data.Assets[SNOGroup.Scene][this.SceneSNO.Id].Data as Mooege.Common.MPQ.FileFormats.Scene;
+            if (data == null) return;
+
+            this.AABBBounds = data.AABBBounds;
+            this.AABBMarketSetBounds = data.AABBMarketSetBounds;
+            this.NavMesh = data.NavMesh;
+            this.MarkerSets = data.MarkerSets;
+            this.LookLink = data.LookLink;
+            this.NavZone = data.NavZone;
         }
 
         #endregion
