e1a51d546e502a098e46b79d8b62039128036429
RedBlueKnight <janiszewski.krzysztof@yahoo.pl>
2012-04-27 21:34:03 +0200

    Removed hardcoded monsters powers. Powers should be taken from MPQ now

--- src/Mooege/Core/GS/Actors/Implementations/Monsters/Monsters.cs
+++ src/Mooege/Core/GS/Actors/Implementations/Monsters/Monsters.cs
@@ -25,6 +25,7 @@ using Mooege.Core.GS.Common.Types.TagMap;
 using Mooege.Core.GS.AI.Brains;
 using Mooege.Net.GS.Message;
 
+
 namespace Mooege.Core.GS.Actors.Implementations.Monsters
 {
     #region TreasureGoblin
@@ -36,10 +37,6 @@ namespace Mooege.Core.GS.Actors.Implementations.Monsters
             : base(world, snoId, tags)
         {
             this.Brain = new MonsterBrain(this);
-            (Brain as MonsterBrain).AddPresetPower(105371);
-            (Brain as MonsterBrain).AddPresetPower(54055);
-            //(Brain as MonsterBrain).AddPresetPower(54836); //ThrowPortal
-            //(Brain as MonsterBrain).AddPresetPower(105665); //ThrowPortal_Fast
             //this.Attributes[GameAttribute.Hitpoints_Max_Total] = 5f;
             this.Attributes[GameAttribute.Hitpoints_Max] = 5f;
             //this.Attributes[GameAttribute.Hitpoints_Total_From_Level] = 0f;
@@ -60,7 +57,6 @@ namespace Mooege.Core.GS.Actors.Implementations.Monsters
             : base(world, snoId, tags)
         {
             this.Brain = new MonsterBrain(this);
-            (Brain as MonsterBrain).AddPresetPower(30525);
             //this.Attributes[GameAttribute.Hitpoints_Max_Total] = 5f;
             this.Attributes[GameAttribute.Hitpoints_Max] = 5f;
             //this.Attributes[GameAttribute.Hitpoints_Total_From_Level] = 0f;
@@ -80,11 +76,6 @@ namespace Mooege.Core.GS.Actors.Implementations.Monsters
             : base(world, snoId, tags)
         {
             this.Brain = new MonsterBrain(this);
-            (Brain as MonsterBrain).AddPresetPower(30005);
-            (Brain as MonsterBrain).AddPresetPower(30001);
-            (Brain as MonsterBrain).AddPresetPower(107729);
-            (Brain as MonsterBrain).AddPresetPower(30592);
-            (Brain as MonsterBrain).AddPresetPower(30550);
             //this.Attributes[GameAttribute.Hitpoints_Max_Total] = 5f;
             this.Attributes[GameAttribute.Hitpoints_Max] = 5f;
             //this.Attributes[GameAttribute.Hitpoints_Total_From_Level] = 0f;
@@ -104,8 +95,6 @@ namespace Mooege.Core.GS.Actors.Implementations.Monsters
             : base(world, snoId, tags)
         {
             this.Brain = new MonsterBrain(this);
-            (Brain as MonsterBrain).AddPresetPower(30592);
-            (Brain as MonsterBrain).AddPresetPower(30244);
             //this.Attributes[GameAttribute.Hitpoints_Max_Total] = 15f;
             this.Attributes[GameAttribute.Hitpoints_Max] = 15f;
             //this.Attributes[GameAttribute.Hitpoints_Total_From_Level] = 0f;
@@ -125,9 +114,6 @@ namespace Mooege.Core.GS.Actors.Implementations.Monsters
             : base(world, snoId, tags)
         {
             this.Brain = new MonsterBrain(this);
-            (Brain as MonsterBrain).AddPresetPower(30580);
-            (Brain as MonsterBrain).AddPresetPower(30581);
-            (Brain as MonsterBrain).AddPresetPower(202344);
             //this.Attributes[GameAttribute.Hitpoints_Max_Total] = 5f;
             this.Attributes[GameAttribute.Hitpoints_Max] = 5f;
             //this.Attributes[GameAttribute.Hitpoints_Total_From_Level] = 0f;
@@ -148,11 +134,6 @@ namespace Mooege.Core.GS.Actors.Implementations.Monsters
             : base(world, snoId, tags)
         {
             this.Brain = new MonsterBrain(this);
-            (Brain as MonsterBrain).AddPresetPower(30596); //Static_Pose/Unique
-            (Brain as MonsterBrain).AddPresetPower(29990); //Static_Pose/Unique 
-            (Brain as MonsterBrain).AddPresetPower(30800); //Unique
-            //if WoodWraith uses 30800, release spores. -> this is only for unique woodwraith.
-
             //this.Attributes[GameAttribute.Hitpoints_Max_Total] = 5f;
             this.Attributes[GameAttribute.Hitpoints_Max] = 5f;
             //this.Attributes[GameAttribute.Hitpoints_Total_From_Level] = 0f;
@@ -173,10 +154,8 @@ namespace Mooege.Core.GS.Actors.Implementations.Monsters
         public Zombie(World world, int snoId, TagMap tags)
             : base(world, snoId, tags)
         {
+            
             this.Brain = new MonsterBrain(this);
-            (Brain as MonsterBrain).AddPresetPower(30550); //Summon_Zombie_Crawler.pow, no idea what this would do.
-            (Brain as MonsterBrain).AddPresetPower(30592);
-            (Brain as MonsterBrain).AddPresetPower(30005);
             //this.Attributes[GameAttribute.Hitpoints_Max_Total] = 5f;
             this.Attributes[GameAttribute.Hitpoints_Max] = 5f;
             //this.Attributes[GameAttribute.Hitpoints_Total_From_Level] = 0f;
@@ -195,7 +174,6 @@ namespace Mooege.Core.GS.Actors.Implementations.Monsters
             : base(world, snoId, tags)
         {
             this.Brain = new MonsterBrain(this);
-            (Brain as MonsterBrain).AddPresetPower(7789); //Weapon_Ranged_Wand.pow?
             //this.Attributes[GameAttribute.Hitpoints_Max_Total] = 4.132813f;
             this.Attributes[GameAttribute.Hitpoints_Max] = 4.132813f;
             //this.Attributes[GameAttribute.Hitpoints_Total_From_Level] = 0f;
@@ -214,7 +192,6 @@ namespace Mooege.Core.GS.Actors.Implementations.Monsters
             : base(world, snoId, tags)
         {
             this.Brain = new MonsterBrain(this);
-            (Brain as MonsterBrain).AddPresetPower(7789); //Weapon_Ranged_Wand.pow?
             //this.Attributes[GameAttribute.Hitpoints_Max_Total] = 5f;
             this.Attributes[GameAttribute.Hitpoints_Max] = 1.602539f;
             //this.Attributes[GameAttribute.Hitpoints_Total_From_Level] = 0f;
@@ -233,12 +210,6 @@ namespace Mooege.Core.GS.Actors.Implementations.Monsters
             : base(world, snoId, tags)
         {
             this.Brain = new MonsterBrain(this);
-            //Not sure how to actually make the AI for the Buffs used for this mob.
-            (Brain as MonsterBrain).AddPresetPower(30290); //InvulnerableDuringBuff.pow
-            (Brain as MonsterBrain).AddPresetPower(79486); //UninterruptibleDuringBuff.pow
-            (Brain as MonsterBrain).AddPresetPower(30582); //UntargetableDuringBuff.pow
-            (Brain as MonsterBrain).AddPresetPower(225599); //CannotDieDuringBuff.pow
-            //this.Attributes[GameAttribute.Buff_Active,488] = true; //Can we handle buffs?.
             //this.Attributes[GameAttribute.Hitpoints_Max_Total] = 4.132813f;
             this.Attributes[GameAttribute.Hitpoints_Max] = 4.132813f;
             //this.Attributes[GameAttribute.Hitpoints_Total_From_Level] = 0f;
@@ -258,8 +229,6 @@ namespace Mooege.Core.GS.Actors.Implementations.Monsters
             : base(world, snoId, tags)
         {
             this.Brain = new MonsterBrain(this);
-            (Brain as MonsterBrain).AddPresetPower(30592);
-            (Brain as MonsterBrain).AddPresetPower(30005);
             //this.Attributes[GameAttribute.Hitpoints_Max_Total] = 5f;
             this.Attributes[GameAttribute.Hitpoints_Max] = 5f;
             //this.Attributes[GameAttribute.Hitpoints_Total_From_Level] = 0f;
@@ -278,7 +247,6 @@ namespace Mooege.Core.GS.Actors.Implementations.Monsters
             : base(world, snoId, tags)
         {
             this.Brain = new MonsterBrain(this);
-            (Brain as MonsterBrain).AddPresetPower(30592);
             //this.Attributes[GameAttribute.Hitpoints_Max_Total] = 5f;
             this.Attributes[GameAttribute.Hitpoints_Max] = 5f;
             //this.Attributes[GameAttribute.Hitpoints_Total_From_Level] = 0f;
@@ -298,11 +266,6 @@ namespace Mooege.Core.GS.Actors.Implementations.Monsters
             : base(world, snoId, tags)
         {
             this.Brain = new MonsterBrain(this);
-            (Brain as MonsterBrain).AddPresetPower(30503);
-            (Brain as MonsterBrain).AddPresetPower(29990);
-            (Brain as MonsterBrain).AddPresetPower(30001);
-            (Brain as MonsterBrain).AddPresetPower(30005);
-            (Brain as MonsterBrain).AddPresetPower(30543); //Summon Skeletons
             //this.Attributes[GameAttribute.Hitpoints_Max_Total] = 5f;
             this.Attributes[GameAttribute.Hitpoints_Max] = 5f;
             //this.Attributes[GameAttribute.Hitpoints_Total_From_Level] = 0f;
@@ -321,8 +284,6 @@ namespace Mooege.Core.GS.Actors.Implementations.Monsters
             : base(world, snoId, tags)
         {
             this.Brain = new MonsterBrain(this);
-            (Brain as MonsterBrain).AddPresetPower(30495);
-            (Brain as MonsterBrain).AddPresetPower(30334);
             //this.Attributes[GameAttribute.Hitpoints_Max_Total] = 5f;
             this.Attributes[GameAttribute.Hitpoints_Max] = 5f;
             //this.Attributes[GameAttribute.Hitpoints_Total_From_Level] = 0f;
@@ -342,10 +303,6 @@ namespace Mooege.Core.GS.Actors.Implementations.Monsters
             : base(world, snoId, tags)
         {
             this.Brain = new MonsterBrain(this);
-            (Brain as MonsterBrain).AddPresetPower(30474);
-            (Brain as MonsterBrain).AddPresetPower(30473);
-            (Brain as MonsterBrain).AddPresetPower(30592);
-            (Brain as MonsterBrain).AddPresetPower(30005);
             //this.Attributes[GameAttribute.Hitpoints_Max_Total] = 5f;
             this.Attributes[GameAttribute.Hitpoints_Max] = 5f;
             //this.Attributes[GameAttribute.Hitpoints_Total_From_Level] = 0f;
@@ -367,10 +324,6 @@ namespace Mooege.Core.GS.Actors.Implementations.Monsters
             : base(world, snoId, tags)
         {
             this.Brain = new MonsterBrain(this);
-            (Brain as MonsterBrain).AddPresetPower(30531);
-            (Brain as MonsterBrain).AddPresetPower(30530);
-            //(Brain as MonsterBrain).AddPresetPower(30529); //Explode
-            (Brain as MonsterBrain).AddPresetPower(30592);
             //this.Attributes[GameAttribute.Hitpoints_Max_Total] = 5f;
             this.Attributes[GameAttribute.Hitpoints_Max] = 5f;
             //this.Attributes[GameAttribute.Hitpoints_Total_From_Level] = 0f;
@@ -390,8 +343,6 @@ namespace Mooege.Core.GS.Actors.Implementations.Monsters
             : base(world, snoId, tags)
         {
             this.Brain = new MonsterBrain(this);
-            (Brain as MonsterBrain).AddPresetPower(30334);
-            (Brain as MonsterBrain).AddPresetPower(30592);
             //this.Attributes[GameAttribute.Hitpoints_Max_Total] = 5f;
             this.Attributes[GameAttribute.Hitpoints_Max] = 5f;
             //this.Attributes[GameAttribute.Hitpoints_Total_From_Level] = 0f;
@@ -411,9 +362,6 @@ namespace Mooege.Core.GS.Actors.Implementations.Monsters
             : base(world, snoId, tags)
         {
             this.Brain = new MonsterBrain(this);
-            (Brain as MonsterBrain).AddPresetPower(110518); //spit
-            (Brain as MonsterBrain).AddPresetPower(94734); // Vommit Without the Spawns
-            (Brain as MonsterBrain).AddPresetPower(30592); //Instant Wep
             //this.Attributes[GameAttribute.Hitpoints_Max_Total] = 13.38281f;
             this.Attributes[GameAttribute.Hitpoints_Max] = 13.38281f;
             //this.Attributes[GameAttribute.Hitpoints_Total_From_Level] = 0f;
