31ae9161abc9ee9026136dc66dddbd8f13c86554
Iker Ruiz Arnauda <iker_arnauda@hotmail.com>
2012-06-15 15:21:08 -0500

    Update master

--- contrib/mpq-fix/fix-mpq.bat
+++ contrib/mpq-fix/fix-mpq.bat
@@ -5,4 +5,6 @@ mkdir base
 wget.exe -P base http://ak.worldofwarcraft.com.edgesuite.net/d3-pod-retail/NA/8370.direct/Data_D3/PC/MPQs/base/d3-update-base-9558.MPQ
 wget.exe -P base http://ak.worldofwarcraft.com.edgesuite.net/d3-pod-retail/NA/8370.direct/Data_D3/PC/MPQs/base/d3-update-base-9749.MPQ
 wget.exe -P base http://ak.worldofwarcraft.com.edgesuite.net/d3-pod-retail/NA/8370.direct/Data_D3/PC/MPQs/base/d3-update-base-9858.MPQ
+wget.exe -P base http://ak.worldofwarcraft.com.edgesuite.net/d3-pod-retail/NA/8370.direct/Data_D3/PC/MPQs/base/d3-update-base-9950.MPQ
+wget.exe -P base http://ak.worldofwarcraft.com.edgesuite.net/d3-pod-retail/NA/8370.direct/Data_D3/PC/MPQs/base/d3-update-base-9991.MPQ
 wget -P base http://ak.worldofwarcraft.com.edgesuite.net/d3-pod-retail/NA/8370.direct/Data_D3/PC/MPQs/base/d3-update-base-9950.MPQ
